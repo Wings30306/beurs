0ae7e1847ca8339c6d6058a82f25695e40528330
Joke Heyndels <jo_hannah@outlook.com>
2020-06-27 19:00:12 +0200

    Add models in "Producten": Type & Product.

---
+++ producten/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Type(models.Model):
+    naam = models.CharField(max_length=15)
+    taks = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return self.naam + " - " + str(self.taks) + "%"
+
+class Product(models.Model):
+    isin = models.CharField(max_length=12, unique=True)
+    ticker = models.CharField(max_length=5, primary_key=True, unique=True)
+    naam = models.CharField(max_length=30)
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    dividend = models.BooleanField()
+
+    def __str__(self):
+        return self.ticker
