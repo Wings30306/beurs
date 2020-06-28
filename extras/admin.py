fd1ccb30fef23e6b379a15786ebf60316383a09f
Joke Heyndels <jo_hannah@outlook.com>
2020-06-28 12:17:06 +0200

    Register new models to admin

--- extras/admin.py
+++ extras/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import ManueleCorrectie, Schikking, WinstDeelname, Splitsing
 # Register your models here.
+admin.site.register(ManueleCorrectie)
+admin.site.register(Schikking)
+admin.site.register(WinstDeelname)
+admin.site.register(Splitsing)
