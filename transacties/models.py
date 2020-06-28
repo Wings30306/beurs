a5dad51e7c631e9dc2a717260dcdabb576ee48ef
Joke Heyndels <jo_hannah@outlook.com>
2020-06-28 11:47:10 +0200

    Remove model "Correctie" from Transacties app

--- transacties/models.py
+++ transacties/models.py
@@ -33,11 +33,3 @@ class Dividend(models.Model):
 
     def __str__(self):
         str(self.datum) + " - " + self.product.ticker
-
-
-class Correctie(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    bedrag = models.DecimalField(max_digits=9, decimal_places=2)
-
-    def __str__(self):
-        str(self.datum) + " - " + self.product.ticker
